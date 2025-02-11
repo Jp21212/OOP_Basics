bcb9f9e2a9550886f11c86ca044027169fcba799
Jp21212 <Jamesp2814@tuckahoeschools.org>
2025-02-11 13:29:10 +0000

    Initial commit

---
+++ fruits.py
@@ -0,0 +1,32 @@
+class Fruit:
+    def__init__(self, name, color, price):
+        self.name = name
+        self.color = red
+        self.price = float(price)
+
+    def details(self):
+        print{f"""
+             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Fruit Type: {self.name}
+            Fruit color: {self.color}
+            Fruit cost: {self.price}
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        """}
+
+    def calc_sales_tax(self, tax):
+        total = (self.price*tax) + self price
+        print(f"""
+        #####################
+        Total Cost
+        #####################
+        """)
+
+
+apple = Fruit("Apple", "Red", "100")
+pear = Fruit("Pear", "Green" "132.54")
+banana = Fruit("Banana", "Yellow" "2000000000")
+
+apple.details()
+pear.details()
+
+apple.calc_sales_tax(0.4)
